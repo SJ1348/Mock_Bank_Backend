45614696937ece9adcb8d01bc80a5563d217dc9a
SJ1348 <sidthokal@gmail.com>
2024-01-02 16:09:26 +0530

    Wallet updateBalance Route

--- routes/wallet.js
+++ routes/wallet.js
@@ -13,6 +13,35 @@ router.post("/getBalance", userInputvalidation, async (req, res) => {
   });
 });
 
-router.post("/updateBalance", userInputvalidation, async (req, res) => {});
+router.put("/updateBalance", userInputvalidation, async (req, res) => {
+  const accountNumber = req.body.accountNumber;
+  const amount = req.body.amount;
+  const operation = req.body.operation;
+  if (operation === "credit") {
+    let account = await Account.updateOne(
+      { accountNumber: accountNumber },
+      { $inc: { balance: amount } }
+    );
+    res.json({
+      message: "Balance Updated",
+    });
+  } else if (operation === "debit") {
+    let account = await Account.findOne({ accountNumber: accountNumber });
+
+    if (account.balance < amount) {
+      res.status(500).json({
+        message: "Insufficient funds",
+      });
+    } else {
+      await Account.updateOne(
+        { accountNumber: accountNumber },
+        { $inc: { balance: -amount } }
+      );
+      res.json({
+        message: "Balance Updated",
+      });
+    }
+  }
+});
 
 export { router };
