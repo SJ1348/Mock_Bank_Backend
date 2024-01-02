1ed6a5e21babba98ca9d7cd3f3616702cf7ca8db
manjiripathak16 <manjiripathak01@gmail.com>
2024-01-02 15:05:47 +0530

    Added getBalance logic

--- db/index.js
+++ db/index.js
@@ -2,13 +2,14 @@ import mongoose from "mongoose";
 
 // Connect to MongoDB
 mongoose.connect(
-  "mongodb+srv://manjiripathak01:DbowYNgfWacekh9Y@mockbank.hyvpzb6.mongodb.net/"
+  "mongodb+srv://manjiripathak01:DbowYNgfWacekh9Y@mockbank.hyvpzb6.mongodb.net/Accounts"
 );
 
 //Define Schemas
 const AccountSchema = new mongoose.Schema({
   accountNumber: Number,
   pin: Number,
+  balance: Number,
 });
 
 const Account = mongoose.model("Account", AccountSchema);
--- routes/wallet.js
+++ routes/wallet.js
@@ -6,14 +6,15 @@ const router = Router();
 // Wallet Routes
 router.post("/getBalance", async (req, res) => {
   const pin = req.body.pin;
+  const accountNumber = req.body.accountNumber;
 
-  Account.create({
-    pin: pin,
-  });
+  let account = await Account.find({ accountNumber: accountNumber, pin: pin });
 
-  res.json({
-    message: "Account created successfully",
-  });
+  if (account) {
+    await Account.findOne({ pin: pin }).then((acc) => {
+      res.json({ balance: acc.balance });
+    });
+  }
 });
 
 router.post("/updateBalance", async (req, res) => {
